e7a10bd85956856321e1b0389c11b2ec2f84ffbf
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-01-25 14:10:32 +0100

    first commit - original code

---
+++ src/simplegithubdemo/SimpleGitHubDemo.java
@@ -0,0 +1,20 @@
+/*
+Testing GitHub
+ */
+package simplegithubdemo;
+
+/**
+ *
+ * @author FVanderSchueren
+ */
+public class SimpleGitHubDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Simply hello");
+    }
+    
+}
