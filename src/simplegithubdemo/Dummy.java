6d3a34ebd874cc2e231bf1f6608e4f5465c2974e
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-01-25 14:23:58 +0100

    first commit - Dummy class

---
+++ src/simplegithubdemo/Dummy.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplegithubdemo;
+
+/**
+ *
+ * @author FVanderSchueren
+ */
+public class Dummy {
+    private String text;
+
+    public Dummy(String text) {
+        this.text = text;
+    }
+    
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    
+    
+}
